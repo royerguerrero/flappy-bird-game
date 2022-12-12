065368ba21b273dfddd0c457b61dd76c38ac634d
Royer G <royjuni3431@gmail.com>
2022-12-12 16:23:51 -0500

    feat: instance a react project and add fancy bird

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './flappy-bird.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Flappy Bird 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
